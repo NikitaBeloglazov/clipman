a4a2e7758bc25f425737a1c6aeb37b4ffc6890ec
Nikita Beloglazov <77229847+NikitaBeloglazov@users.noreply.github.com>
2023-09-29 01:53:39 +0300

    Move to src, add bulding to pypi

---
+++ src/clipman/__version__.py
@@ -0,0 +1,7 @@
+"""
+Created for storing version number.
+The version number will be placed here based on the tag in Github using the tags_marker.py script.
+
+Having "!!{PLACEHOLDER}!!" in final releases is NOT allowed.
+"""
+__version__="!!{PLACEHOLDER}!!"
